8a8f726dffe11a5bcb8671a9c95c20c0364d0884
Logan <logan@lcthr.com>
2026-09-11 17:52:53 -0400

    Add display brightness keys to FUN layer
    INC left of the right-hand home-row shift, DEC directly below it,
    matching FUN's right-thumb activation (mods share the activating
    hand, functional keys the opposite hand -- same convention as
    MEDIA's left-pinky brightness column).

--- config/miryoku/custom_config.h
+++ config/miryoku/custom_config.h
@@ -4,6 +4,9 @@
 // Prospector display brightness action codes.
 // Used in the keymap as &pbl PBL_INC etc. (dongle+prospector builds only).
 // Non-prospector builds see U_NA via the fallback macros below.
+// Bound on MEDIA (left-hand inner-index column for INC/DEC, right-hand
+// inner-index for TOG) and on FUN, immediately left of the right-hand
+// home-row shift (INC on the home row, DEC directly below it).
 #define PBL_TOG 0
 #define PBL_INC 1
 #define PBL_DEC 2
@@ -18,6 +21,12 @@
 #define U_PBL_INC U_NA
 #define U_PBL_DEC U_NA
 #endif
+
+#define MIRYOKU_LAYER_FUN \
+&kp F12,           &kp F7,            &kp F8,            &kp F9,            &kp PSCRN,         U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp F11,           &kp F4,            &kp F5,            &kp F6,            &kp SLCK,          U_PBL_INC,         &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp F10,           &kp F1,            &kp F2,            &kp F3,            &kp PAUSE_BREAK,   U_PBL_DEC,         &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp K_APP,         &kp SPACE,         &kp TAB,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_MEDIA \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      &studio_unlock,    U_PBL_TOG,         U_RGB_EFF,         U_RGB_HUI,         U_RGB_SAI,         U_RGB_BRI,         \
